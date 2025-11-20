acaa51a0bb5f5152417b51ec04f98e6b0354f0be
Joel Yovera <ingyovera6114@gmail.com>
2025-11-19 23:38:12 -0500

    integración de fontawesome para los íconos

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,12 +1,20 @@
 import { ApplicationConfig, provideBrowserGlobalErrorListeners, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
-
 import { routes } from './app.routes';
+import { provideIonicAngular } from '@ionic/angular/standalone';
+
+//IMPORTAR LA LIBRERIA DEL FONTAWESOME PARA LOS ICONOS 
+import { FaIconLibrary } from '@fortawesome/angular-fontawesome';
+import { fas } from '@fortawesome/pro-solid-svg-icons';
+import { far } from '@fortawesome/pro-regular-svg-icons';
+import { fal } from '@fortawesome/pro-light-svg-icons';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
     provideZoneChangeDetection({ eventCoalescing: true }),
-    provideRouter(routes)
+    provideRouter(routes),
+    provideIonicAngular({}),
+    FaIconLibrary
   ]
 };
